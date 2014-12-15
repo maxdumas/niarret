558ef6f06b0c5245a88c9ec1addee59a9c5b62b8
Max Dumas <maltor124@gmail.com>
2014-12-15 12:12:15 -0500

    Removed freeze from FPS controls because it was driving me crazy.

--- js/vendor/FirstPersonControls.js
+++ js/vendor/FirstPersonControls.js
@@ -139,9 +139,6 @@ THREE.FirstPersonControls = function ( object, domElement ) {
 
 			case 82: /*R*/ this.moveUp = true; break;
 			case 70: /*F*/ this.moveDown = true; break;
-
-			case 81: /*Q*/ this.freeze = !this.freeze; break;
-
 		}
 
 	};
